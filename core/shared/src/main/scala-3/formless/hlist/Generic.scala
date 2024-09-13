00fe5ebbfecdf1342414d2badebc03418a669219
Matt Dziuban <mrdziuban@gmail.com>
2024-09-13 13:00:29 -0400

    Revert `Generic.Inst` and `LabelledGeneric.Inst` changes so implicit `Aux` params resolve.

--- core/shared/src/main/scala-3/formless/hlist/Generic.scala
+++ core/shared/src/main/scala-3/formless/hlist/Generic.scala
@@ -2,31 +2,34 @@ package formless.hlist
 
 import scala.deriving.Mirror
 
-trait Generic[A] extends Serializable {
-  type Repr
-  def to(a: A): Repr
-  def from(r: Repr): A
-}
+trait Generic[A] extends Generic.Inst[A]
 
 object Generic {
   type Aux[A, R] = Generic[A] { type Repr = R }
 
   inline def apply[A](using g: Generic[A]): Generic.Aux[A, g.Repr] = g
 
-  given fromInst[A, R](using i: Inst[A, R]): Generic.Aux[A, R] =
+  given fromInst[A](using i: Inst[A]): Generic.Aux[A, i.Repr] =
     new Generic[A] {
-      type Repr = R
+      type Repr = i.Repr
       def from(r: Repr): A = i.from(r)
       def to(a: A): Repr = i.to(a)
     }
 
-  final class Inst[A, R](val from: R => A, val to: A => R) extends Serializable
+  sealed trait Inst[A] extends Serializable {
+    type Repr
+    def from(r: Repr): A
+    def to(a: A): Repr
+  }
 
   object Inst {
-    inline given productInst[A <: Product](using m: Mirror.ProductOf[A]): Inst[A, HList.FromTuple[m.MirroredElemTypes]] =
-      Inst(
-        r => m.fromTuple(HList.toTuple(r).asInstanceOf[m.MirroredElemTypes]),
-        a => HList.fromTuple(Tuple.fromProductTyped(a)),
-      )
+    type Aux[A, R] = Inst[A] { type Repr = R }
+
+    given productInst[A <: Product](using m: Mirror.ProductOf[A]): Inst.Aux[A, HList.FromTuple[m.MirroredElemTypes]] =
+      new Inst[A] {
+        type Repr = HList.FromTuple[m.MirroredElemTypes]
+        def to(a: A): Repr = HList.fromTuple(Tuple.fromProductTyped(a))
+        def from(r: Repr): A = m.fromTuple(HList.toTuple(r).asInstanceOf[m.MirroredElemTypes])
+      }
   }
 }
